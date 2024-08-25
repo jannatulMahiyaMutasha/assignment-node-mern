c20d2e399f62869842dc1badefc1f45ed947c87b
jannatulMahiyaMutasha <mutashamahiya678@gmail.com>
2024-08-26 04:21:06 +0600

    some changes

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-const fs= require('fs');
-const http= require('http');
 
-const server = http.createServer(function (req, res) {
+const http = require('http');
+const fs = require('fs');
 
-    if(req.url==="/"){
-        let data= fs.readFileSync('home.html','utf8');
-        res.end(data);
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+    // Handle different routes based on the URL
+    if (req.url === '/') {
+        // Home Page
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('This is the Home Page');
     }
-    else if(req.url==="/About"){
 
-        let data= fs.readFileSync('about.html','utf8');
-        res.end(data);
+    else if (req.url === '/About') {
+        // About Page
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('This is the About Page');
     }
-    else if(req.url==="/Contact"){
-        let data= fs.readFileSync('contact.html','utf8');
-        res.end(data);
 
+    else if (req.url === '/Contact') {
+        // Contact Page
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('This is the Contact Page');
+    }
+
+    else if (req.url === '/file-write') {
+        // File Write Operation
+        fs.writeFile('demo.txt', 'hello world', (err) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error writing file');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end('File written successfully');
+            }
+        });
+
+    } else {
+        // 404 Not Found
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Page not found');
     }
 });
 
-server.listen(5500);
-console.log("it is listening on port 5500");
+// Start the server and listen on port 5500
+server.listen(5500, () => {
+    console.log('Server is listening on port 5500');
+});
